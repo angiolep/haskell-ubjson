fd911379156ca5a0172cf65b00956504070af143
Paolo Angioletti <paolo.angioletti@gmail.com>
2017-07-14 23:08:59 +0100

    Encode text

--- src/Data/Ubjson.hs
+++ src/Data/Ubjson.hs
@@ -1,10 +1,13 @@
-module Data.Ubjson (encode, pack) where
+module Data.Ubjson (encode) where
 
+--import           Prelude
+import qualified Data.Text               as T
+import           Data.Text.Encoding      --(encodeUtf8Builder)
 import qualified Data.ByteString.Lazy    as L
-import           Data.ByteString.Builder
-import           Data.Monoid
-import           Data.Word
-import           Data.Int
+import           Data.ByteString.Builder --(Builder, toLazyByteString, charUtf8, int8, word8)
+import           Data.Monoid             --((<>))
+import           Data.Word               --(Word8)
+import           Data.Int                --(Int8)
 
 class Ubjson a where
   pack :: a -> Builder
@@ -14,38 +17,41 @@ instance Ubjson Bool where
   pack False = charUtf8 'F'
 
 instance Ubjson Char where
-  pack a = charUtf8 'C' <> charUtf8 a
+  pack x = charUtf8 'C' <> charUtf8 x
 
 instance Ubjson Word8 where
-  pack a = charUtf8 'U' <> word8 a
+  pack x = charUtf8 'U' <> word8 x
 
 instance Ubjson Int8 where
-  pack a = charUtf8 'i' <> int8 a
+  pack x = charUtf8 'i' <> int8 x
 
 -- TODO Num types here ...
 
-instance Ubjson Integer where
-  pack a
-    | a >= 0     && a <= 2^8  -1 = pack ((fromIntegral a) :: Word8)
-    | a >= -2^7  && a <= 2^7  -1 = pack ((fromIntegral a) :: Int8)
---    | a >= -2^15 && a <= 2^15 -1 = charUtf8 'I' <> int16 ((fromIntegral a) :: Int16)
+instance Ubjson Int where
+  pack x
+    | x >= 0     && x <= 2^8  -1 = pack ((fromIntegral x) :: Word8)
+    | x >= -2^7  && x <= 2^7  -1 = pack ((fromIntegral x) :: Int8)
+--    | x >= -2^15 && x <= 2^15 -1 = charUtf8 'I' <> int16 ((fromIntegral x) :: Int16)
     | otherwise               = undefined
 
 
--- TODO instance Ubjson Text where
+instance Ubjson T.Text where
+  pack x =
+    let len = T.length x
+    in charUtf8 'S' <> pack len <> encodeUtf8Builder x
 
 instance Ubjson a => Ubjson (Maybe a) where
-  pack b = case b of
+  pack x = case x of
     Nothing -> charUtf8 'Z'
-    Just a  -> pack a
+    Just y  -> pack y
 
---instance Ubjson a => Ubjson [a] where =>
---  pack []     = charUtf8 '[' <> charUtf8 ']'
---  pack (x:xs) = pack x <> pack xs
--- foldr <> 0
+instance (Ubjson a) => Ubjson [a] where
+  pack []     = undefined
+  pack (x:xs) = pack x <> pack xs
+
 
 encode :: Ubjson a => a -> L.ByteString
-encode a = toLazyByteString $ pack a
+encode x = toLazyByteString $ pack x
 
 
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,3 +1,5 @@
+
+import qualified Data.Text as T
 import qualified Data.ByteString.Lazy as L
 import           Prelude hiding (words)
 import           Test.Hspec
@@ -18,25 +20,32 @@ main = hspec $ do
     it "should pack (False :: Bool) as Ubjson false" $ do
       (L.unpack $ encode False) `shouldBe` words [0x46]
 
-    it "should pack ('a' :: Char) as Ubjson char" $ do
-      (L.unpack $ encode 'a') `shouldBe` ([0x43, 0x61] :: [Word8])
+    it "should pack ('c' :: Char) as Ubjson char" $ do
+      (L.unpack $ encode 'c') `shouldBe`  words [0x43, 0x63]
 
-    it "should pack (163 :: Integer) as Ubjson uint8" $ do
-      (L.unpack $ encode (163 :: Integer)) `shouldBe` words [0x55, 0xA3]
+    it "should pack (163 :: Int) as Ubjson uint8" $ do
+      (L.unpack $ encode (163 :: Int)) `shouldBe` words [0x55, 0xA3]
 
-    it "should pack (-93 :: Integer) as Ubjson int8" $ do
-      (L.unpack $ encode (-93 :: Integer)) `shouldBe` words [0x69, 0xA3]
+    it "should pack (-93 :: Int) as Ubjson int8" $ do
+      (L.unpack $ encode (-93 :: Int)) `shouldBe` words [0x69, 0xA3]
 
-    --it "should pack (64789 :: Integer) as Ubjson int16" $ do
-    --      (L.unpack $ encode (64789 :: Integer)) `shouldBe` words [0x69, 0xA3]
+    --it "should pack (64789 :: Int) as Ubjson int16" $ do
+    --      (L.unpack $ encode (64789 :: Int)) `shouldBe` words [0x69, 0xA3]
 
     -- TODO more numerics here to be tested ...
+    -- decimal : 5
+    -- binary  : 0000 0101
+    -- hex     : 0x05
+
+    it "should pack (\"hello\" :: Text) as Ubjson string" $ do
+      -- Note that it cannot be any Nothing, but rather a constrained one
+      (L.unpack $ encode (T.pack "hello")) `shouldBe` words [0x53, 0x55, 0x05, 0x68, 0x65, 0x6C, 0x6C, 0x6F]
 
     it "should pack (Nothing :: Maybe) as Ubjson null" $ do
       -- Note that it cannot be any Nothing, but rather a constrained one
-      (L.unpack $ encode (Nothing :: Maybe Bool)) `shouldBe` [0x5A :: Word8]
+      (L.unpack $ encode (Nothing :: Maybe Bool)) `shouldBe` words [0x5A]
 
-    it "should pack Just 'A' :: Maybe" $ do
+    it "should pack Just 'c' :: Maybe as Ubjson char" $ do
       -- Note that it cannot be any Nothing, but rather a constrained one
-      (L.unpack $ encode (Just 'a')) `shouldBe` ([0x43, 0x61] :: [Word8])
+      (L.unpack $ encode (Just 'c')) `shouldBe` words [0x43, 0x63]
 
