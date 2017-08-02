c0859bb868ed1556ea2a6b24014c73788d1d8ead
Paolo Angioletti <paolo.angioletti@gmail.com>
2017-08-02 08:47:41 +0100

    Encode data maps to Ubjson objects

--- src/Data/Ubjson.hs
+++ src/Data/Ubjson.hs
@@ -1,4 +1,8 @@
-{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE
+		OverloadedStrings
+  , ExistentialQuantification
+  , FlexibleInstances
+#-}
 
 module Data.Ubjson (
     encode
@@ -9,14 +13,14 @@ module Data.Ubjson (
 where
 
 --import           Prelude
-import qualified Data.Text               as T
-import           Data.Text.Encoding      --(encodeUtf8Builder)
 import qualified Data.ByteString.Lazy    as L
 import           Data.ByteString.Builder --(Builder, toLazyByteString, charUtf8, word8, int8, int16BE, int32BE, int64BE)
-import           Data.Monoid             --((<>))
-import           Data.Word               --(Word8)
 import           Data.Int                --(Int8, Int16)
-import           Data.Dynamic
+import qualified Data.Map                as M
+import           Data.Monoid             --((<>))
+import qualified Data.Text               as T
+import           Data.Text.Encoding      --(encodeUtf8Builder)
+import           Data.Word               --(Word8)
 
 
 data Noop = Noop
@@ -24,67 +28,86 @@ data Noop = Noop
 data Null = Null
 
 class ToUbjson a where
-  pack :: a -> Builder
+  toEncoding :: a -> Builder
 
 instance ToUbjson Null where
-  pack Null = charUtf8 'Z'
+  toEncoding Null = charUtf8 'Z'
 
 instance ToUbjson Noop where
-  pack Noop = charUtf8 'N'
+  toEncoding Noop = charUtf8 'N'
 
 instance ToUbjson Bool where
-  pack True  = charUtf8 'T'
-  pack False = charUtf8 'F'
+  toEncoding True  = charUtf8 'T'
+  toEncoding False = charUtf8 'F'
 
 instance ToUbjson Char where
-  pack x = charUtf8 'C' <> charUtf8 x
+  toEncoding x = charUtf8 'C' <> charUtf8 x
 
 instance ToUbjson Word8 where
-  pack x = charUtf8 'U' <> word8 x
+  toEncoding x = charUtf8 'U' <> word8 x
 
 instance ToUbjson Int8 where
-  pack x = charUtf8 'i' <> int8 x
+  toEncoding x = charUtf8 'i' <> int8 x
 
 instance ToUbjson Int16 where
-  pack x = charUtf8 'I' <> int16BE x
+  toEncoding x = charUtf8 'I' <> int16BE x
 
 instance ToUbjson Int32 where
-  pack x = charUtf8 'l' <> int32BE x
+  toEncoding x = charUtf8 'l' <> int32BE x
 
 instance ToUbjson Int64 where
-  pack x = charUtf8 'L' <> int64BE x
+  toEncoding x = charUtf8 'L' <> int64BE x
 
 instance ToUbjson Int where
-  pack x
-    | x >= 0     && x <= 2^8  -1 = pack ((fromIntegral x) :: Word8)
-    | x >= -2^7  && x <= 2^7  -1 = pack ((fromIntegral x) :: Int8)
-    | x >= -2^15 && x <= 2^15 -1 = pack ((fromIntegral x) :: Int16)
-    | x >= -2^31 && x <= 2^31 -1 = pack ((fromIntegral x) :: Int32)
-    | x >= -2^63 && x <= 2^63 -1 = pack ((fromIntegral x) :: Int64)
+  toEncoding x
+    | x >= 0     && x <= 2^8  -1 = toEncoding ((fromIntegral x) :: Word8)
+    | x >= -2^7  && x <= 2^7  -1 = toEncoding ((fromIntegral x) :: Int8)
+    | x >= -2^15 && x <= 2^15 -1 = toEncoding ((fromIntegral x) :: Int16)
+    | x >= -2^31 && x <= 2^31 -1 = toEncoding ((fromIntegral x) :: Int32)
+    | x >= -2^63 && x <= 2^63 -1 = toEncoding ((fromIntegral x) :: Int64)
     | otherwise                  = undefined
 
 
+textToEncoding :: T.Text -> Builder
+textToEncoding x =
+  let b = encodeUtf8Builder x
+      l = (fromIntegral $ L.length $ toLazyByteString b) :: Int
+  in toEncoding l <> b
+
+
 instance ToUbjson T.Text where
-  pack x =
-    let builder = encodeUtf8Builder x
-        len = (fromIntegral $ L.length $ toLazyByteString builder) :: Int
-    in charUtf8 'S' <> pack len <> builder
+  toEncoding x = charUtf8 'S' <> textToEncoding x
+
 
 instance ToUbjson a => ToUbjson (Maybe a) where
-  pack x = case x of
+  toEncoding x = case x of
     Nothing -> charUtf8 'Z'
-    Just y  -> pack y
+    Just y  -> toEncoding y
+
 
 instance (ToUbjson a) => ToUbjson [a] where
-  pack [] = charUtf8 '[' <> charUtf8 ']'
-  pack xs = charUtf8 '[' <> mconcat (map pack xs) <> charUtf8 ']'
+  toEncoding [] = charUtf8 '[' <> charUtf8 ']'
+  toEncoding xs = charUtf8 '[' <> mconcat (map toEncoding xs) <> charUtf8 ']'
 
+-- TODO Try to optimize "mconcat (map toEncoding xs)" with a foldl
 
 data Ubj = forall a. ToUbjson a => Ubj a
 
+
 instance ToUbjson Ubj where
-  pack (Ubj a) = pack a
+  toEncoding (Ubj a) = toEncoding a
+
+-- type Dict v = M.Map T.Text v
+
+-- instance ToUbjson v => ToUbjson (Dict v) where
+instance ToUbjson v => ToUbjson (M.Map T.Text v) where
+  toEncoding m =
+    let s = charUtf8 '{'
+        f b k v = b <> textToEncoding k <> toEncoding v
+        e = charUtf8 '}'
+    in (M.foldlWithKey f s m) <> e
+    -- TODO How about invoking strict fold? Why left and not right?
 
 
 encode :: ToUbjson a => a -> L.ByteString
-encode x = toLazyByteString $ pack x
+encode x = toLazyByteString $ toEncoding x
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,7 @@
-import qualified Data.Text as T
-import qualified Data.ByteString.Lazy as L
+
+import qualified Data.Text            as T
+import qualified Data.Map             as M
+import qualified Data.ByteString.Lazy as B
 import           Prelude
 import           Test.Hspec
 import           Data.Ubjson
@@ -19,7 +21,7 @@ main = hspec $ do
     -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     it "should encode Noop as Ubjson noop" $ do
-      let actual = L.unpack $ encode Noop
+      let actual = B.unpack $ encode Noop
       actual `shouldBe` expected [0x4E]
 
 
@@ -28,7 +30,7 @@ main = hspec $ do
     -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     it "should encode Null as Ubjson null" $ do
-      let actual = L.unpack $ encode Null
+      let actual = B.unpack $ encode Null
       actual `shouldBe` expected [0x5A]
 
 
@@ -37,11 +39,11 @@ main = hspec $ do
     -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     it "should encode (True :: Bool) as Ubjson true" $ do
-      let actual = L.unpack $ encode True
+      let actual = B.unpack $ encode True
       actual `shouldBe` expected [0x54]
 
     it "should encode (False :: Bool) as Ubjson false" $ do
-      let actual = L.unpack $ encode False
+      let actual = B.unpack $ encode False
       actual `shouldBe` expected [0x46]
 
 
@@ -50,7 +52,7 @@ main = hspec $ do
     -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     it "should encode ('a' :: Char) as Ubjson char" $ do
-      let actual = L.unpack $ encode 'a'
+      let actual = B.unpack $ encode 'a'
       actual `shouldBe`  expected [0x43, 0x61]
 
 
@@ -59,35 +61,35 @@ main = hspec $ do
     -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     it "should encode (163 :: Int) as Ubjson uint8" $ do
-      let actual = L.unpack $ encode (163 :: Int)
+      let actual = B.unpack $ encode (163 :: Int)
       actual `shouldBe` expected [0x55, 0xA3]
 
     it "should encode (-93 :: Int) as Ubjson int8" $ do
-      let actual = L.unpack $ encode (-93 :: Int)
+      let actual = B.unpack $ encode (-93 :: Int)
       actual `shouldBe` expected [0x69, 0xA3]
 
     it "should encode (256 :: Int) as Ubjson int16" $ do
-      let actual = L.unpack $ encode (256 :: Int)
+      let actual = B.unpack $ encode (256 :: Int)
       actual `shouldBe` expected [0x49, 0x01, 0x00]
 
     it "should encode (32767 :: Int) as Ubjson int16" $ do
-      let actual = L.unpack $ encode (32767 :: Int)
+      let actual = B.unpack $ encode (32767 :: Int)
       actual `shouldBe` expected [0x49, 0x7F, 0xFF]
 
     it "should encode (32768 :: Int) as Ubjson int32" $ do
-      let actual = L.unpack $ encode (32768 :: Int)
+      let actual = B.unpack $ encode (32768 :: Int)
       actual `shouldBe` expected [0x6C, 0x00, 0x00, 0x80, 0x00]
 
     it "should encode (2147483647 :: Int) as Ubjson int32" $ do
-      let actual = L.unpack $ encode (2147483647 :: Int)
+      let actual = B.unpack $ encode (2147483647 :: Int)
       actual `shouldBe` expected [0x6C, 0x7F, 0xFF, 0xFF, 0xFF]
 
     it "should encode (2147483648 :: Int) as Ubjson int64" $ do
-      let actual = L.unpack $ encode (2147483648 :: Int)
+      let actual = B.unpack $ encode (2147483648 :: Int)
       actual `shouldBe` expected [ 0x4C, 0x00, 0x00, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00 ]
 
 --    it "should encode (9223372036854775807 :: Int) as Ubjson int64" $ do
---      let actual = (L.unpack $ encode (9223372036854775807 :: Int))
+--      let actual = (B.unpack $ encode (9223372036854775807 :: Int))
 --      actual `shouldBe` expected [ 0x4C, 0x00, 0x00, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00 ]
 
 
@@ -96,19 +98,19 @@ main = hspec $ do
     -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     it "should encode (\"hello\" :: Text) as Ubjson string" $ do
-      let actual = L.unpack $ encode (T.pack "hello")
+      let actual = B.unpack $ encode (T.pack "hello")
       --                             S     U     5     h     e     l     l     0
       actual `shouldBe` expected [0x53, 0x55, 0x05, 0x68, 0x65, 0x6C, 0x6C, 0x6F]
 
 
     it "should encode (\"привет\" :: Text) as Ubjson string" $ do
-      let actual = L.unpack $ encode (T.pack "привет")
+      let actual = B.unpack $ encode (T.pack "привет")
       --                             S     U    12           п           р           и           в           е           т
       actual `shouldBe` expected [0x53, 0x55, 0x0C, 0xD0, 0xBF, 0xD1, 0x80, 0xD0, 0xB8, 0xD0, 0xB2, 0xD0, 0xB5, 0xD1, 0x82]
 
 
     it "should encode (\"مرحبا\" :: Text) as Ubjson string" $ do
-      let actual = L.unpack $ encode (T.pack "مرحبا")
+      let actual = B.unpack $ encode (T.pack "مرحبا")
       --                             S     U    10           ا            ب           ح           ر           م
       actual `shouldBe` expected [0x53, 0x55, 0x0A, 0xD9, 0x85, 0xD8, 0xB1, 0xD8, 0xAD, 0xD8, 0xA8, 0xD8, 0xA7]
 
@@ -118,12 +120,12 @@ main = hspec $ do
     -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     it "should encode (Nothing :: Maybe) as Ubjson null" $ do
-      let actual = L.unpack $ encode (Nothing :: Maybe Bool)
+      let actual = B.unpack $ encode (Nothing :: Maybe Bool)
       actual `shouldBe` expected [0x5A]
 
 
     it "should encode (Just 'c' :: Maybe) as Ubjson char" $ do
-      let actual = L.unpack $ encode (Just 'c')
+      let actual = B.unpack $ encode (Just 'c')
       actual `shouldBe` expected [0x43, 0x63]
 
 
@@ -132,15 +134,15 @@ main = hspec $ do
     -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     it "should encode ['a', 'b', 'c'] as Ubjson array" $ do
-      let actual = L.unpack $ encode ['a','b','c']
+      let actual = B.unpack $ encode ['a','b','c']
       actual `shouldBe` expected [0x5B, 0x43, 0x61, 0x43, 0x62, 0x43, 0x63, 0x5D]
 
     it "should encode ([163, -93] :: Int) as Ubjson array" $ do
-      let actual = L.unpack $ encode ([163, -93] :: [Int])
+      let actual = B.unpack $ encode ([163, -93 :: Int])
       actual `shouldBe` expected [0x5B, 0x55, 0xA3, 0x69, 0xA3, 0x5D]
 
     it "should encode [True, False, True, True] as Ubjson array" $ do
-      let actual = L.unpack $ encode [True, False, True, True]
+      let actual = B.unpack $ encode [True, False, True, True]
       actual `shouldBe` expected [0x5B, 0x54, 0x46, 0x54, 0x54, 0x5D]
 
 
@@ -150,6 +152,19 @@ main = hspec $ do
 
     it "should encode heterogeneous list as Ubjson array" $ do
       let hlist = [Ubj Noop, Ubj Null, Ubj True, Ubj 'a', Ubj (163 :: Int), Ubj $ T.pack "hello"]
-      let actual = L.unpack $ encode hlist
+      let actual = B.unpack $ encode hlist
       actual `shouldBe` expected [0x5B, 0x4E, 0x5A, 0x54, 0x43, 0x61, 0x55, 0xA3, 0x53, 0x55, 0x05, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x5D]
+
+
+    -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    -- Maps
+    -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    it "should encode M.fromList [(T.pack $ \"age\", 42 :: Int)] as Ubjson object" $ do
+      let m = M.fromList [(T.pack $ "age", 42 :: Int)]
+      let actual = B.unpack $ encode m
+      actual `shouldBe` expected [0x7B, 0x55, 0x03, 0x61, 0x67, 0x65, 0x55, 0x2A, 0x7D]
+
+
+
 
